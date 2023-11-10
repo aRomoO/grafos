064428a4a07db9f97d88c62429dadabc55e45489
frediromo <frediromo@gmail.com>
2023-11-10 09:19:13 -0600

    Initial commit

---
+++ MyGraph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fredi on 10/11/23.
+//
+
+#include "MyGraph.h"
---
+++ MyGraph.h
@@ -0,0 +1,14 @@
+//
+// Created by fredi on 10/11/23.
+//
+
+#ifndef GRAFOS_MYGRAPH_H
+#define GRAFOS_MYGRAPH_H
+
+
+class MyGraph {
+
+};
+
+
+#endif //GRAFOS_MYGRAPH_H
