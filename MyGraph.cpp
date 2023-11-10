057873c010456c5259dffc656b9827f60d5923d0
frediromo <frediromo@gmail.com>
2023-11-10 10:47:22 -0600

    added constructor

--- MyGraph.cpp
+++ MyGraph.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "MyGraph.h"
+
+MyGraph::MyGraph(std::vector<std::vector<int>> &matriz)
+{
+    this->matriz = matriz;
+}
+
+void MyGraph::loadGraph(std::vector<std::vector<int>> &matriz)
+{
+
+}
+
+void MyGraph::DFS(int nodo)
+{
+
+}
+
+void MyGraph::BFS(int nodo)
+{
+
+}
--- MyGraph.h
+++ MyGraph.h
@@ -4,10 +4,17 @@
 
 #ifndef GRAFOS_MYGRAPH_H
 #define GRAFOS_MYGRAPH_H
-
+#include "vector"
 
 class MyGraph {
+private:
+    std::vector<std::vector<int>> matriz;
 
+public:
+    MyGraph(std::vector<std::vector<int>>& matriz);
+    void loadGraph(std::vector<std::vector<int>>& matriz);
+    void DFS(int nodo);
+    void BFS(int nodo);
 };
 
 
