7ca7fd926e67961661bcc3df4b3ce03b0767ecc9
Alfredo Romo Osorno <frediromo@gmail.com>
2023-11-12 19:48:54 -0600

    Added Time complexity

--- MyGraph.h
+++ MyGraph.h
@@ -14,8 +14,9 @@ public:
     MyGraph(std::vector<std::vector<int>>& matriz); //O(1)
     void loadGraph(std::vector<std::vector<int>>& matriz); //O(1)
     void DFS(int nodo);
-    void DFS(int nodo, std::vector<bool>& visited); //Helper function para DFS recursivo
-    void BFS(int nodo);
+    void DFS(int nodo, std::vector<bool>& visited); //O(n) | O(V+E) V: Vertices , E: Nodos
+                                                    // Helper function para DFS recursivo
+    void BFS(int nodo); //O(n) | O(V+E) V: Vertices , E: Nodos
 };
 
 
